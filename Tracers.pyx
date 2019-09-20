100458419d7643ab4b548a87ed4f79c7a6cc85cf
JH <jiahe.gatech@gmail.com>
2019-09-19 20:37:25 -0700

    fix tracer criterion for updraft

--- Tracers.pyx
+++ Tracers.pyx
@@ -981,7 +981,7 @@ cdef updraft_indicator_sc_w_ql(Grid.DimStruct *dims,  double *tracer_raw, double
                     for j in xrange(dims.nlg[1]):
                         jshift = j*jstride
                         ijk = ishift + jshift + k
-                        tracer_normed[ijk] = 0.0
+                        tracer_normed[ijk] = copysign( (tracer_raw[ijk] - mean[k])/ sigma_min, w_half)
             else:
                for i in xrange(dims.nlg[0]):
                     ishift = i*istride
